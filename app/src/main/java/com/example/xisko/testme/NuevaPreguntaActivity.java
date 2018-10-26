9e5469e03c4217c0adb04f5861d7d0abd4954bd4
Fran <xisko12@gmail.com>
2018-10-26 13:18:07 +0200

    Añadido control de todos los campos en pregunta nueva

--- app/src/main/java/com/example/xisko/testme/NuevaPreguntaActivity.java
+++ app/src/main/java/com/example/xisko/testme/NuevaPreguntaActivity.java
@@ -1,5 +1,6 @@
 package com.example.xisko.testme;
 
+import android.graphics.Color;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class NuevaPreguntaActivity extends AppCompatActivity {
 
@@ -27,16 +29,109 @@ public class NuevaPreguntaActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText pregunta = findViewById(R.id.titulo);
+               final EditText  pregunta = findViewById(R.id.titulo);
+               final EditText correcta = findViewById(R.id.titulo2);
+               final EditText incorrecta1 = findViewById(R.id.titulo3);
+               final EditText incorrecta2 = findViewById(R.id.titulo4);
+               final EditText incorrecta3 = findViewById(R.id.titulo5);
                 if(pregunta.getText().toString().isEmpty()){
-                    pregunta.setBackgroundColor(0xFF00FF00);
+                    pregunta.setBackgroundColor(Color.rgb(255,64,64));
 
                     Snackbar.make(v, "Comprueba que todos los campos esten rellenos", Snackbar.LENGTH_LONG)
                             .show();
 
+                } if (correcta.getText().toString().isEmpty()){
+
+                    correcta.setBackgroundColor(Color.rgb(255,64,64));
+
+                    Snackbar.make(v, "Comprueba que todos los campos esten rellenos", Snackbar.LENGTH_LONG)
+                            .show();
+
+
+                } if (incorrecta1.getText().toString().isEmpty()){
+
+                    incorrecta1.setBackgroundColor(Color.rgb(255,64,64));
+
+                    Snackbar.make(v, "Comprueba que todos los campos esten rellenos", Snackbar.LENGTH_LONG)
+                            .show();
+
+
+                }if (incorrecta2.getText().toString().isEmpty()){
+
+                    incorrecta2.setBackgroundColor(Color.rgb(255,64,64));
+
+                    Snackbar.make(v, "Comprueba que todos los campos esten rellenos", Snackbar.LENGTH_LONG)
+                            .show();
+
+
+                }if (incorrecta3.getText().toString().isEmpty()){
+
+                    incorrecta3.setBackgroundColor(Color.rgb(255,64,64));
+
+                    Snackbar.make(v, "Comprueba que todos los campos esten rellenos", Snackbar.LENGTH_LONG)
+                            .show();
+
+
                 }
 
+                pregunta.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View view, boolean hasFocus) {
+                        if (hasFocus) {
+
+                            pregunta.setBackgroundColor(Color.rgb(250,250,250));
+
+                        }
+                    }
+                });
+
+                correcta.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View view, boolean hasFocus) {
+                        if (hasFocus) {
+
+                            correcta.setBackgroundColor(Color.rgb(250,250,250));
+
+                        }
+                    }
+                });
+
+                incorrecta1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View view, boolean hasFocus) {
+                        if (hasFocus) {
+
+                            incorrecta1.setBackgroundColor(Color.rgb(250,250,250));
+
+                        }
+                    }
+                });
+
+                incorrecta2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View view, boolean hasFocus) {
+                        if (hasFocus) {
+
+                            incorrecta2.setBackgroundColor(Color.rgb(250,250,250));
+
+                        }
+                    }
+                });
+
+                incorrecta3.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View view, boolean hasFocus) {
+                        if (hasFocus) {
+
+                            incorrecta3.setBackgroundColor(Color.rgb(250,250,250));
+
+                        }
+                    }
+                });
+
             }
+
+
         });
 
 
